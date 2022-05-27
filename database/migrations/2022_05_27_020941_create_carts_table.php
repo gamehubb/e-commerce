86c73f02280a59d855ad9302a04a877670b7a6a8
santoshgtm95 <santosh.gtm6@gmail.com>
2022-05-27 16:55:53 +0630

    [commit] carts impl and issue fixed

---
+++ database/migrations/2022_05_27_020941_create_carts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('product_id');
+            $table->text('product_name');
+            $table->text('category_id');
+            $table->text('image')->nullable();
+            $table->integer('quantity');
+            $table->string('price');
+            $table->float('total_amount');
+            $table->string('color');
+            $table->text('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
